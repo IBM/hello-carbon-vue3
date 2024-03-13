5104a6ce89d38bc6797ce56a7a22d84d650659eb
David Nixon <david_nixon@us.ibm.com>
2024-03-13 08:35:59 -0400

    docs: update readme to highlight additions
    Signed-off-by: David Nixon <david_nixon@us.ibm.com>

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  purge: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
-  content: [],
+  content: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
   theme: {
     extend: {
       colors: {

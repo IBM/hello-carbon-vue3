ba7132a363a74042ef52a00155c0ab1c689216ee
David Nixon <dwnixon@gmail.com>
2025-04-13 11:50:29 -0400

    feat: build-a-bug working
    Signed-off-by: David Nixon <dwnixon@gmail.com>

--- src/views/BuildABugView.vue
+++ src/views/BuildABugView.vue
@@ -1,13 +1,9 @@
 <script setup>
 import { computed, ref } from "vue";
 import { useTranslation } from "i18next-vue";
-import {
-  OpenPanelBottom32 as BottomIncomplete,
-  OpenPanelFilledBottom32 as BottomComplete,
-  OpenPanelTop32 as TopIncomplete,
-  OpenPanelFilledTop32 as TopComplete,
-  Reset32 as Reset,
-} from "@carbon/icons-vue";
+import { Reset16 as Reset } from "@carbon/icons-vue";
+import "@carbon/web-components/es/components/icon-button/index.js";
+import "@carbon/web-components/es/components/progress-indicator/index.js";
 
 const { /** @type function(string) */ t } = useTranslation();
 
@@ -19,12 +15,6 @@ const bugParts = [
   new URL("@/assets/build-a-bug/saw-stag.png", import.meta.url).href,
   new URL("@/assets/build-a-bug/tiger-beetle.png", import.meta.url).href,
 ];
-const step = ref(0);
-function onStepClicked(val) {
-  if (val.uid === "bug-head") step.value = 0;
-  else if (val.uid === "bug-body") step.value = 1;
-  else if (val.uid === "bug-bottom") step.value = 2;
-}
 
 const topIndex = ref(-1);
 function top(val) {
@@ -61,129 +51,132 @@ function clearAll() {
   middleIndex.value = -1;
   bottomIndex.value = -1;
 }
+
+const step = computed(() => {
+  if (middleIndex.value > -1) return "bottom";
+  else if (topIndex.value > -1) return "middle";
+  return "top";
+});
+
+const topState = computed(() => {
+  if (topIndex.value > -1) return "complete";
+  else if (step.value === "top") return "current";
+  else return "incomplete";
+});
+
+const middleState = computed(() => {
+  if (middleIndex.value > -1) return "complete";
+  else if (step.value === "middle") return "current";
+  else return "incomplete";
+});
+
+const bottomState = computed(() => {
+  if (bottomIndex.value > -1) return "complete";
+  else if (step.value === "bottom") return "current";
+  else return "incomplete";
+});
+
 </script>
 
 <template>
-  <cv-grid>
-    <cv-row>
-      <cv-column>
-        <div class="text-3xl py-8">
-          {{ t("build-a-bug") }}
-        </div>
-        <cv-icon-button
-          label="reset"
-          :icon="Reset"
-          class="mb-5"
-          @click="clearAll"
+  <div class="w-full pl-14 mt-12">
+    <div>
+      <div class="text-3xl! py-8">
+        {{ t("build-a-bug") }}
+      </div>
+      <cds-icon-button
+        class="mb-5"
+        @click="clearAll"
+      >
+        <Reset slot="icon" />
+        <span slot="tooltip-content">reset</span>
+      </cds-icon-button>
+      <cds-progress-indicator
+        :vertical="false"
+        :space-equally="true"
+      >
+        <cds-progress-step
+          :label="t('top')"
+          :state="topState"
+          :secondary-label-text="t('choose-top')"
         />
-        <cv-progress
-          :vertical="false"
-          :space-equally="true"
-          :initial-step="step"
+        <cds-progress-step
+          :label="t('middle')"
+          :state="middleState"
+          :secondary-label-text="t('choose-middle')"
+        />
+        <cds-progress-step
+          :label="t('bottom')"
+          :state="bottomState"
+          :secondary-label-text="t('choose-bottom')"
+        />
+      </cds-progress-indicator>
+    </div>
+    <div>
+      <div class="mt-9">
+        <section
+          v-if="step === 'top'"
+          class="flex justify-between"
         >
-          <cv-progress-step
-            id="bug-head"
-            :label="t('top')"
-            :complete="topIndex > -1"
-            :additional-info="t('choose-top')"
-            @step-clicked="onStepClicked"
-          >
-            <template #step-icon>
-              <top-complete v-if="topIndex > -1" />
-              <top-incomplete v-else />
-            </template>
-          </cv-progress-step>
-          <cv-progress-step
-            id="bug-body"
-            :label="t('middle')"
-            :complete="middleIndex > -1"
-            :additional-info="t('choose-middle')"
-            @step-clicked="onStepClicked"
-          />
-          <cv-progress-step
-            id="bug-bottom"
-            :label="t('bottom')"
-            :complete="bottomIndex > -1"
-            :additional-info="t('choose-bottom')"
-            @step-clicked="onStepClicked"
-          >
-            <template #step-icon>
-              <bottom-complete v-if="bottomIndex > -1" />
-              <bottom-incomplete v-else />
-            </template>
-          </cv-progress-step>
-        </cv-progress>
-      </cv-column>
-    </cv-row>
-    <cv-row>
-      <cv-column>
-        <div class="mt-9">
-          <section
-            v-if="step === 0"
-            class="flex justify-between"
-          >
-            <div
-              v-for="(url, index) in bugParts"
-              :key="`url-top-${index}`"
-              class="h-[31px] w-[105px] bg-top bg-carbon-blue-20 mt-3 cursor-pointer bg-cover"
-              :style="{ backgroundImage: `url(${url})` }"
-              @click="top(index)"
-            />
-          </section>
-          <section
-            v-if="step === 1"
-            class="flex justify-between"
-          >
-            <div
-              v-for="(url, index) in bugParts"
-              :key="`url-middle-${index}`"
-              class="h-[31px] w-[105px] bg-center bg-carbon-blue-20 mt-3 cursor-pointer bg-cover"
-              :style="{ backgroundImage: `url(${url})` }"
-              @click="middle(index)"
-            />
-          </section>
-          <section
-            v-if="step === 2"
-            class="flex justify-between"
-          >
-            <div
-              v-for="(url, index) in bugParts"
-              :key="`url-middle-${index}`"
-              class="h-[31px] w-[105px] bg-bottom bg-carbon-blue-20 mt-3 cursor-pointer bg-cover"
-              :style="{ backgroundImage: `url(${url})` }"
-              @click="bottom(index)"
-            />
-          </section>
-        </div>
-      </cv-column>
-    </cv-row>
-    <cv-row>
-      <cv-column>
-        <div class="mt-9">
           <div
-            class="w-[422px] h-[125px] bg-carbon-blue-20 bg-top"
-            :style="topUrl"
+            v-for="(url, index) in bugParts"
+            :key="`url-top-${index}`"
+            class="h-[31px] w-[105px] bg-top bg-carbon-blue-20 mt-3 cursor-pointer bg-cover"
+            :style="{ backgroundImage: `url(${url})` }"
+            @click="top(index)"
           />
+        </section>
+        <section
+          v-if="step === 'middle'"
+          class="flex justify-between"
+        >
           <div
-            class="w-[422px] h-[125px] bg-center"
-            :class="{
-              'bg-carbon-blue-40': middleIndex === -1,
-              'bg-carbon-blue-20': middleIndex !== -1,
-            }"
-            :style="middleUrl"
+            v-for="(url, index) in bugParts"
+            :key="`url-middle-${index}`"
+            class="h-[31px] w-[105px] bg-center bg-carbon-blue-20 mt-3 cursor-pointer bg-cover"
+            :style="{ backgroundImage: `url(${url})` }"
+            @click="middle(index)"
           />
+        </section>
+        <section
+          v-if="step === 'bottom'"
+          class="flex justify-between"
+        >
           <div
-            class="w-[422px] h-[125px] bg-bottom"
-            :class="{
-              'bg-carbon-blue-60': bottomIndex === -1,
-              'bg-carbon-blue-20': bottomIndex !== -1,
-            }"
-            :style="bottomUrl"
+            v-for="(url, index) in bugParts"
+            :key="`url-middle-${index}`"
+            class="h-[31px] w-[105px] bg-bottom bg-carbon-blue-20 mt-3 cursor-pointer bg-cover"
+            :style="{ backgroundImage: `url(${url})` }"
+            @click="bottom(index)"
           />
-        </div>
-      </cv-column>
-    </cv-row>
-  </cv-grid>
+        </section>
+      </div>
+    </div>
+    <div>
+      <div class="mt-9">
+        <div
+          class="w-[422px] h-[125px] bg-carbon-blue-20 bg-top"
+          :style="topUrl"
+        />
+        <div
+          class="w-[422px] h-[125px] bg-center"
+          :class="{
+            'bg-carbon-blue-40': middleIndex === -1,
+            'bg-carbon-blue-20': middleIndex !== -1,
+          }"
+          :style="middleUrl"
+        />
+        <div
+          class="w-[422px] h-[125px] bg-bottom"
+          :class="{
+            'bg-carbon-blue-60': bottomIndex === -1,
+            'bg-carbon-blue-20': bottomIndex !== -1,
+          }"
+          :style="bottomUrl"
+        />
+      </div>
+    </div>
+  </div>
 </template>
 
 <style scoped></style>
